a8cdbc77792d9a4729b81cf46f0023f74de3e17b
oliveiradniel <kadadniel@gmail.com>
2024-12-13 06:13:43 -0300

    refactor(eslint): configure eslint in the latest version

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,ts}"] },
+  { ignores: ["**/node_modules", ".git", "dist"] },
+  {
+    rules: {
+      semi: ["error", "always"], // requires the semicolon
+      quotes: ["error", "single"], // requires single quotes
+      "no-console": ["warn"], // notice if there is `console.log`
+      eqeqeq: ["error", "always"], // require use the `===` instead of `==`
+      "no-unused-vars": ["warn"], // warning about unused variables
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
